44483e51825448295736e37bec09b6bcf6fcc4d3
Fiza Saleem <159792382+Fizasaleem358@users.noreply.github.com>
2024-04-02 00:23:15 +0500

    updated

---
+++ dist/task-16.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+// task-16
+let dinnerGuestList = ["hurma", "saba", "iqra", "ayzal"];
+console.log(dinnerGuestList);
+console.log(`we have 3 more friends to invite on dinner`);
+// adding three more friends
+//ading at begginnng
+dinnerGuestList.unshift("fiza");
+// ["fiza","hurma", "saba", "iqra", "ayzal"]
+console.log(dinnerGuestList);
+//add at middle 
+dinnerGuestList.splice(2, 0, "arisha");
+console.log(dinnerGuestList);
+//at at last
+dinnerGuestList.push("eshal");
+console.log(dinnerGuestList);
+//print all
+for (let i = 0; i < dinnerGuestList.length; i++) {
+    console.log(`Mrs ${dinnerGuestList[i]} you are invited to our dinner`);
+}
+export {};
---
+++ src/task-20.ts
@@ -0,0 +1,2 @@
+#! /usr/bin/env node
+// task-20
